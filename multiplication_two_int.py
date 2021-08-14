16e6f81a7e8061d2ac75efcab0f830480e14813f
alberjose <alberjose.romero@gmail.com>
2021-08-14 09:25:21 -0600

    multiplication of two numbers integer without using *, only sum

---
+++ multiplication_two_int.py
@@ -0,0 +1,9 @@
+a = int(input('Set the first integer number: '))
+b = int(input('Se the second integer number: '))
+
+result = 0
+
+for x in range(a):
+    result += b 
+
+print('The multiplication result is: ',result)
