c293ee9e81cad0f0afe092af67834cfe30ca27d6
alberjose <alberjose.romero@gmail.com>
2021-08-14 11:33:15 -0600

    this is a tool for reverse name in python

---
+++ reverse_full_name.py
@@ -0,0 +1,8 @@
+name = input('Please, set the name: ')
+lastname = input('Please, set the last name: ')
+
+fullname = name + ' ' + lastname
+reverse_name = fullname[::-1]
+
+print('The fullname is: ',fullname)
+print('the reverse name is:', reverse_name)
